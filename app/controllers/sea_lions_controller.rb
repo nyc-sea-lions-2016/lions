33eed60c59beb41f295dc1f222d6f947523ab1b6
Sam Blackman <sblackman@gmail.com>
2016-03-24 09:57:02 -0400

    finish sea lions routes, add many skills

--- app/controllers/sea_lions_controller.rb
+++ app/controllers/sea_lions_controller.rb
@@ -11,12 +11,17 @@ end
 
 # Sea Lions new action
 get '/sea_lions/new' do
+  @skills = Skill.all
   erb :'/sea_lions/new'
 end
 
 # Sea Lions create action
 post '/sea_lions' do
-  sea_lion = SeaLion.create(params)
+  skills_records = params[:skills].map { |skill_id| Skill.find(skill_id) }
+  sea_lion = SeaLion.create(name: params[:name],
+                            ocean: params[:ocean],
+                            whisker_count: params[:whisker_count],
+                            skills: skills_records)
   redirect "/sea_lions/#{sea_lion.id}"
 end
 
@@ -24,4 +29,24 @@ end
 get '/sea_lions/:id' do
   @sea_lion = SeaLion.find(params[:id])
   erb :'/sea_lions/show'
+end
+
+# Sea Lions edit action
+get '/sea_lions/:id/edit' do
+  @sea_lion = SeaLion.find(params[:id])
+  erb :'/sea_lions/edit'
+end
+
+# Sea Lions update action
+put '/sea_lions/:id' do
+  sea_lion = SeaLion.find(params[:id])
+  sea_lion.update(params[:sea_lion])
+  redirect "/sea_lions/#{sea_lion.id}"
+end
+
+# Sea Lions destroy action
+delete '/sea_lions/:id' do
+  sea_lion = SeaLion.find(params[:id])
+  sea_lion.destroy
+  redirect '/sea_lions'
 end
