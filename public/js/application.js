ef43046b459c1348254aca4f8c81a7a58559e18c
Zack Mance <zmance@gmail.com>
2016-03-29 10:51:44 -0400

    Adds JS intro code

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,40 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+ $(document).ready(function(){
+  
+  $('.see-lion').on('click',function(event){
+    event.preventDefault();
+    alert('you clicked me :( ');
+  });
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+  $('#edit-lion').on('submit',function(event){
+    event.preventDefault();
+    console.log('form submit')
+    debugger;
+  });
+
+  $('#save').on('submit',function(event){
+    event.preventDefault();
+    console.log('button submit')
+    debugger;
+  });
+
+  $('#save').on('click',function(event){
+    console.log('button click')
+  });
+
 });
+
+// // First comes the selector
+// $('')
+
+// //setup the event listener 
+// .on
+
+
+// //tell the method, on what event you want to do something.
+// .on('click')
+
+// //tell the method, what you want to do when that event happens
+// .on('click', function(event){})
+
+
